870f88db25001022398c5c39ce1371f2278c3ef6
Andrés Ujpán <andres04u@gmail.com>
2022-07-14 02:10:19 -0600

    Nuevos Cambios (otra vez ✌️😂)

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,8 +52,8 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 2000,
+    delay: 200,
 })
 
 sr.reveal(`.profile__border`)
